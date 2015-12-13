97c65af03505f63650c7c55f407ffd80056ddde9
thebjorn <bp@datakortet.no>
2015-12-13 15:39:37 +0100

    unify version numbers.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dkfileutils'
-copyright = u'2014 Bjorn Pettersen'
+copyright = u'2015 Bjorn Pettersen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 from distutils.core import setup, Command
 from setuptools.command.test import test as TestCommand
 
-version = '0.1.0'
+version = '1.0.1'
 
 
 class PyTest(TestCommand):
