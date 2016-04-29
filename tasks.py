b09cff56dce4e826615c74f669eb4378bf30abb7
thebjorn <bp@datakortet.no>
2016-04-29 22:07:51 +0200

    upversion.

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
-from dktasklib.version import version, upversion
-from dktasklib.publish import publish
+from invoke import Collection
+from dktasklib import version
+from dktasklib import upversion
+from dktasklib import publish
 
+
+ns = Collection(version, upversion, publish)
