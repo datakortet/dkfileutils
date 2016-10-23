96d268f01b704de3b3a000ce3e7686c9ce495683
thebjorn <bp@datakortet.no>
2016-10-23 21:57:21 +0200

    upversion

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools
 from distutils.core import setup, Command
 from setuptools.command.test import test as TestCommand
 
-version = '1.1.5'
+version = '1.1.6'
 
 
 class PyTest(TestCommand):
