c0643e294263ee0205d465f24121cdd3a33c4861
thebjorn <bp@datakortet.no>
2023-11-30 15:22:04 +0100

    tagged-upversion

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import setuptools
 from distutils.core import setup, Command
 from setuptools.command.test import test as TestCommand
 
-version = '1.4.5'
+version = '1.4.6'
 
 
 class PyTest(TestCommand):
